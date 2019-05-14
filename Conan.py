32b0e769b81160f0613eafdf4f3508dd4fb1a33d
David <1193115470@qq.com>
2019-05-14 23:00:57 +0800

    老师，其实我觉得问题不是您没讲清楚，而是二进制打开之后它横纵坐标写的不清楚。我一直以为50那一行 是从50 一直到5E.......所以54在50这一行的第四个

--- Conan.py
+++ Conan.py
@@ -1,39 +1,93 @@
 import struct
 
 
+
+
+
 def tamper(student_id):
-  pass
+  IDlist=[]
+
+  for no in student_id:
+      IDlist.append(int(no))
+
+  with open('lenna.bmp','r+b') as f:
+      f.seek(54)
+      count=1
+      for number in IDlist:
+          if(number==0):
+            number=10
+          if(count==1):
+            f.read(3*number)
+            f.write(bytes([0,0,0]))
+          else:
+            f.read(3*(number-1))
+            f.write(bytes([0,0,0]))
+          count+=1
+
+
+
+
 
 
 def detect():
+
   with open('lenna.bmp', 'rb') as f:
+
     bmp_file_header = f.read(14)
+
+
 
     bm, size, r1, r2, offset = struct.unpack('<2sIHHI', bmp_file_header)
 
+
+
     f.seek(offset)
 
+
+
     count = 12
+
     offset = 0
+
     last_offset = 0
+
     while count > 0:
+
       color = f.read(3)
+
+
 
       if color == b'\x00\x00\x00':
 
+
+
         if offset - last_offset == 10:
+
           print(0)
+
         else:
+
           print(offset - last_offset)
 
+
+
         last_offset = offset
+
         count -= 1
+
+
 
       offset += 1
 
 
+
+
+
 if __name__ == '__main__':
+
   import sys
   tamper(sys.argv[1])
+
+
 
   detect()
